fd1bee53ffa1b2ea4c8e3d3b7fd163a01eb5c6e6
Paul Yeo <paulyeo21@gmail.com>
2021-11-22 01:27:35 -0800

    Implement consistent hashing

--- loadbalancer/strategy.go
+++ loadbalancer/strategy.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"hash/fnv"
 	"math"
 	"math/rand"
 	"net/http"
+	"sort"
 	"sync"
 )
 
 type strategy interface {
+	add(handlers ...*handler)
+	afterResponse(h *handler, r *http.Request)
 	nextHandler(r *http.Request) *handler
-	afterResponse(h *handler)
 }
 
 type roundRobin struct {
@@ -19,6 +22,8 @@ type roundRobin struct {
 	mu       sync.Mutex
 }
 
+func (s roundRobin) add(handlers ...*handler)                  {}
+func (s roundRobin) afterResponse(h *handler, r *http.Request) {}
 func (s *roundRobin) nextHandler(r *http.Request) *handler {
 	s.mu.Lock()
 	s.last = (s.last + 1) % len(s.handlers)
@@ -26,17 +31,15 @@ func (s *roundRobin) nextHandler(r *http.Request) *handler {
 	return s.handlers[s.last%len(s.handlers)]
 }
 
-func (s roundRobin) afterResponse(h *handler) {}
-
 type random struct {
 	handlers []*handler
 }
 
+func (s random) add(handlers []*handler)                   {}
+func (s random) afterResponse(h *handler, r *http.Request) {}
 func (s random) nextHandler(_ *http.Request) *handler {
 	return s.handlers[rand.Intn(len(s.handlers))]
 }
-
-func (s random) afterResponse(h *handler) {}
 
 type leastConn struct {
 	mu       sync.Mutex
@@ -55,6 +58,14 @@ func newLeastConn(handlers []*handler) *leastConn {
 	}
 }
 
+func (s leastConn) add(handlers ...*handler) {}
+
+func (s *leastConn) afterResponse(h *handler, r *http.Request) {
+	s.mu.Lock()
+	s.handlers[h]--
+	s.mu.Unlock()
+}
+
 func (s *leastConn) nextHandler(_ *http.Request) *handler {
 	var res *handler
 	min := math.MaxInt32
@@ -71,12 +82,6 @@ func (s *leastConn) nextHandler(_ *http.Request) *handler {
 	s.mu.Unlock()
 
 	return res
-}
-
-func (s *leastConn) afterResponse(h *handler) {
-	s.mu.Lock()
-	s.handlers[h]--
-	s.mu.Unlock()
 }
 
 type weightedRoundRobin struct {
@@ -99,6 +104,8 @@ func newWeightedRoundRobin(handlers []*handler) *weightedRoundRobin {
 	}
 }
 
+func (s weightedRoundRobin) add(handlers ...*handler)                  {}
+func (s weightedRoundRobin) afterResponse(h *handler, r *http.Request) {}
 func (s weightedRoundRobin) nextHandler(_ *http.Request) *handler {
 	randInt := rand.Intn(s.ws[len(s.ws)-1] + 1) // rand.Intn is [0, n)
 
@@ -118,22 +125,74 @@ func (s weightedRoundRobin) nextHandler(_ *http.Request) *handler {
 	return s.hs[l]
 }
 
-func (s weightedRoundRobin) afterResponse(h *handler) {}
-
 type simpleHashing struct {
-	hs []*handler
+	handlers []*handler
 }
 
-func (s simpleHashing) hash(userID string) int {
+func (s simpleHashing) add(handlers ...*handler)                  {}
+func (s simpleHashing) afterResponse(h *handler, r *http.Request) {}
+func (s simpleHashing) nextHandler(r *http.Request) *handler {
+	return s.handlers[hash(r.Header.Get("UserID"))%len(s.handlers)]
+}
+
+type consistentHashing struct {
+	handlers    map[int]*handler
+	logs        []string
+	numReplicas int
+	partitions  []int
+}
+
+type consistentHashingOpts struct {
+	handlers    []*handler
+	numReplicas int
+}
+
+func newConsistentHashing(opts *consistentHashingOpts) *consistentHashing {
+	h := &consistentHashing{
+		handlers:    map[int]*handler{},
+		numReplicas: opts.numReplicas,
+		partitions:  []int{},
+	}
+
+	h.add(opts.handlers...)
+
+	return h
+}
+
+func (s *consistentHashing) add(handlers ...*handler) {
+	for _, handler := range handlers {
+		for i := 0; i < s.numReplicas; i++ {
+			h := hash(fmt.Sprintf("%s:%d", handler.name, i))
+			s.partitions = append(s.partitions, h)
+			s.handlers[h] = handler
+		}
+	}
+
+	sort.Ints(s.partitions)
+}
+
+func (s consistentHashing) afterResponse(h *handler, r *http.Request) {
+	s.logs = append(s.logs, r.Header.Get("UserID"))
+}
+
+func (s consistentHashing) nextHandler(r *http.Request) *handler {
+	hash := hash(r.Header.Get("UserID"))
+
+	idx := sort.Search(len(s.handlers), func(i int) bool {
+		return hash < s.partitions[i]
+	})
+
+	if idx == len(s.handlers) {
+		idx = 0
+	}
+
+	return s.handlers[s.partitions[idx]]
+}
+
+func hash(userID string) int {
 	// https://stackoverflow.com/questions/13582519/how-to-generate-hash-number-of-a-string-in-go
 	// https://pkg.go.dev/hash/fnv@go1.17.3
-	h := fnv.New32()
+	h := fnv.New32a()
 	h.Write([]byte(userID))
 	return int(h.Sum32())
 }
-
-func (s simpleHashing) nextHandler(r *http.Request) *handler {
-	return s.hs[s.hash(r.Header.Get("UserID"))%len(s.hs)]
-}
-
-func (s simpleHashing) afterResponse(h *handler) {}
